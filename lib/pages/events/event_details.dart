35857943873674c9acb2d6d106746cad75d38d4c
SawThomasMMK <sawthomas2020@gmail.com>
2024-11-18 14:45:03 +0700

    Runnable project

--- lib/pages/events/event_details.dart
+++ lib/pages/events/event_details.dart
@@ -25,185 +25,189 @@ class _EventDetail extends State<EventDetail> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        // AppBar title with custom styling and color for each word
-        title: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              'Activity',
-              style: TextStyle(
-                  color: Colors.red, fontSize: 20, fontWeight: FontWeight.bold),
-            ),
-            Text(
-              'Tracker',
-              style: TextStyle(
-                  color: Color(0xFFFFD700),
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold),
-            )
-          ],
+        appBar: AppBar(
+          // AppBar title with custom styling and color for each word
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'Activity',
+                style: TextStyle(
+                    color: Colors.red,
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold),
+              ),
+              Text(
+                'Tracker',
+                style: TextStyle(
+                    color: Color(0xFFFFD700),
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold),
+              )
+            ],
+          ),
         ),
-      ),
-      body: Container(
-        // Padding around the form container
-        margin: EdgeInsets.only(left: 20, top: 20, right: 20),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            // Event Name label and input field
-            Text(
-              'Event Name',
-              style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold),
-            ),
-            Container(
-              padding: EdgeInsets.only(left: 5),
-              decoration: BoxDecoration(
-                border: Border.all(),
-                borderRadius: BorderRadius.circular(10),
-              ),
-              child: TextField(
-                controller: nameController,
-                decoration: InputDecoration(border: InputBorder.none),
-              ),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            // Description label and input field
-            Text(
-              'Description',
-              style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold),
-            ),
-            Container(
-              padding: EdgeInsets.only(left: 5),
-              decoration: BoxDecoration(
-                border: Border.all(),
-                borderRadius: BorderRadius.circular(10),
-              ),
-              child: TextField(
-                controller: descriptionController,
-                decoration: InputDecoration(border: InputBorder.none),
-              ),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            // Contact Info label and input field
-            Text(
-              'Contact Info',
-              style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold),
-            ),
-            Container(
-              padding: EdgeInsets.only(left: 5),
-              decoration: BoxDecoration(
-                border: Border.all(),
-                borderRadius: BorderRadius.circular(10),
-              ),
-              child: TextField(
-                controller: contactInfoController,
-                decoration: InputDecoration(border: InputBorder.none),
-              ),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            // Category label and dropdown list
-            Text(
-              'Category',
-              style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold),
-            ),
-            Container(
-              padding: EdgeInsets.only(left: 5, right: 5),
-              decoration: BoxDecoration(
-                border: Border.all(),
-                borderRadius: BorderRadius.circular(10),
-              ),
-              child: DropdownButton<String>(
-                value: selectedCategory,
-                hint: Text('Select a category'),
-                isExpanded: true,
-                icon: Icon(Icons.arrow_downward),
-                style: TextStyle(color: Colors.black, fontSize: 16),
-                onChanged: (String? newValue) {
-                  setState(() {
-                    selectedCategory = newValue;
-                  });
-                },
-                items: [
-                  'Academic',
-                  'Sports & Recreation',
-                  'Arts & Culture',
-                  'Networking & Career Development',
-                  'Leadership & Personal Growth',
-                  'Community Outreach',
-                  'Health & Wellness',
-                  'Technology & Innovation',
-                  'Sustainability & Environment',
-                  'Social & Entertainment'
-                ].map<DropdownMenuItem<String>>((String value) {
-                  return DropdownMenuItem<String>(
-                    value: value,
-                    child: Text(value),
-                  );
-                }).toList(),
-              ),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            // Centered button for submitting event details
-            Center(
-              child: ElevatedButton(
-                onPressed: () async {
-                  // Generate a random ID for the event
-                  String Id = randomAlphaNumeric(10);
-                  // Map to store event details
-                  Map<String, dynamic> eventDetailMap = {
-                    "Name": nameController.text,
-                    "Description": descriptionController.text,
-                    "ID": Id,
-                    "Contact Info": contactInfoController.text,
-                    "Category": selectedCategory, // Store selected category
-                  };
-                  // Add event details to Firestore and show a confirmation toast
-                  await DatabaseMethods()
-                      .addEventDetail(eventDetailMap, Id)
-                      .then((value) {
-                    Fluttertoast.showToast(
-                        msg: "Event detail has been added successfully",
-                        toastLength: Toast.LENGTH_SHORT,
-                        gravity: ToastGravity.CENTER,
-                        timeInSecForIosWeb: 1,
-                        backgroundColor: Colors.red,
-                        textColor: Colors.white,
-                        fontSize: 16.0);
-                  });
-                },
-                child: Text(
-                  'Add',
+        body: SingleChildScrollView(
+          child: Padding(
+            padding:
+                const EdgeInsets.only(left: 20, top: 20, right: 20, bottom: 20),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                // Event Name label and input field
+                Text(
+                  'Event Name',
                   style: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.blueGrey),
+                      color: Colors.black,
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold),
                 ),
-              ),
-            )
-          ],
-        ),
-      ),
-    );
+                Container(
+                  padding: EdgeInsets.only(left: 5),
+                  decoration: BoxDecoration(
+                    border: Border.all(),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: TextField(
+                    controller: nameController,
+                    decoration: InputDecoration(border: InputBorder.none),
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                // Description label and input field
+                Text(
+                  'Description',
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold),
+                ),
+                Container(
+                  padding: EdgeInsets.only(left: 5),
+                  decoration: BoxDecoration(
+                    border: Border.all(),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: TextField(
+                    controller: descriptionController,
+                    decoration: InputDecoration(border: InputBorder.none),
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                // Contact Info label and input field
+                Text(
+                  'Contact Info',
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold),
+                ),
+                Container(
+                  padding: EdgeInsets.only(left: 5),
+                  decoration: BoxDecoration(
+                    border: Border.all(),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: TextField(
+                    controller: contactInfoController,
+                    decoration: InputDecoration(border: InputBorder.none),
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                // Category label and dropdown list
+                Text(
+                  'Category',
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold),
+                ),
+                Container(
+                  padding: EdgeInsets.only(left: 5, right: 5),
+                  decoration: BoxDecoration(
+                    border: Border.all(),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: DropdownButton<String>(
+                    value: selectedCategory,
+                    hint: Text('Select a category'),
+                    isExpanded: true,
+                    icon: Icon(Icons.arrow_downward),
+                    style: TextStyle(color: Colors.black, fontSize: 16),
+                    underline: SizedBox.shrink(),
+                    onChanged: (String? newValue) {
+                      setState(() {
+                        selectedCategory = newValue;
+                      });
+                    },
+                    items: [
+                      'Academic',
+                      'Sports & Recreation',
+                      'Arts & Culture',
+                      'Networking & Career Development',
+                      'Leadership & Personal Growth',
+                      'Community Outreach',
+                      'Health & Wellness',
+                      'Technology & Innovation',
+                      'Sustainability & Environment',
+                      'Social & Entertainment'
+                    ].map<DropdownMenuItem<String>>((String value) {
+                      return DropdownMenuItem<String>(
+                        value: value,
+                        child: Text(value),
+                      );
+                    }).toList(),
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                // Centered button for submitting event details
+                Center(
+                  child: ElevatedButton(
+                    onPressed: () async {
+                      // Generate a random ID for the event
+                      String Id = randomAlphaNumeric(10);
+                      // Map to store event details
+                      Map<String, dynamic> eventDetailMap = {
+                        "Name": nameController.text,
+                        "Description": descriptionController.text,
+                        "ID": Id,
+                        "Contact Info": contactInfoController.text,
+                        "Category": selectedCategory, // Store selected category
+                      };
+                      // Add event details to Firestore and show a confirmation toast
+                      await DatabaseMethods()
+                          .addEventDetail(eventDetailMap, Id)
+                          .then((value) {
+                        Fluttertoast.showToast(
+                            msg: "Event detail has been added successfully",
+                            toastLength: Toast.LENGTH_SHORT,
+                            gravity: ToastGravity.CENTER,
+                            timeInSecForIosWeb: 1,
+                            backgroundColor: Colors.red,
+                            textColor: Colors.white,
+                            fontSize: 16.0);
+                      });
+                    },
+                    child: Text(
+                      'Add',
+                      style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.blueGrey),
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ));
   }
 }
